b394e05c83e6f64c1acdbfdf060975d851a50d87
Tom McFarlin <tom@tommcfarlin.com>
2012-06-07 15:51:23 -0300

    Update master

--- tests/wordpress-tests/unittests-config.php
+++ tests/wordpress-tests/unittests-config.php
@@ -7,7 +7,7 @@ define( 'DB_NAME', 'throwaway' );
 
 /* The usual credentials for a local database. */
 define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', '' );
 define( 'DB_HOST', 'localhost' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
